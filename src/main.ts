78be5b2f3df6533fa3e10fd250897369c9f95c48
楼天楠 <tnlou@justhealth.cn>
2024-06-06 10:25:30 +0800

    chore: 添加pinia持久化方案

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,17 @@ import 'animate.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(createPersistedState())
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,7 +1,15 @@
 import { defineStore } from 'pinia'
 
-
+/**
+ * @see https://prazdevs.github.io/pinia-plugin-persistedstate/zh/
+ */
 export const useLetterStore = defineStore('letter', {
+  persist: {
+    storage: localStorage,
+    key:  'persist-letter',
+    // paths: []
+  },
+
   state: () => ({
 
   }),
